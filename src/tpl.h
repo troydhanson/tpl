c304850005da7be50a92acfff436adefc8564a06
Troy D. Hanson <tdh@tkhanson.net>
2014-12-04 13:16:18 -0500

    Revert "add pointer-sized opaque format character p"
    This reverts commit a95c629f0335e6552f60e3c3a16f87083a66220c.

--- src/tpl.h
+++ src/tpl.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2005-2014, Troy D. Hanson     http://troydhanson.github.io/tpl/
+Copyright (c) 2005-2013, Troy D. Hanson     http://troydhanson.github.com/tpl/
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
